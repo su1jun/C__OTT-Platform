dbe7cc1505a3137c717003fbfdb8b17bf46edb25
su1jun <qaxsd530@gmail.com>
2024-07-21 22:30:51 +0900

    add : navbar 추가

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,8 @@
 import { NextPageContext } from "next"
 import { getSession, signOut } from "next-auth/react"
+
 import useCurrentUser from "@/hooks/useCurrentUser";
+import Navbar from '@/components/Navbar';
 
 export async function getServerSideProps(context: NextPageContext) {
   const session = await getSession(context);
@@ -23,10 +25,13 @@ export default function Home() {
   const { data: user } = useCurrentUser();
 
   return (
-    <div>
-      <h1 className="text-4xl text-green-500">Netflix Clone</h1>
-      <p className="text-white">Logged in as : {user?.email}</p>
-      <button className="h-10 w-full bg-white" onClick={() => signOut()}></button>
-    </div>
+    <>
+      <Navbar />
+      <div>
+        <h1 className="text-4xl text-green-500">Netflix Clone</h1>
+        <p className="text-white">Logged in as : {user?.email}</p>
+        <button className="h-10 w-full bg-white" onClick={() => signOut()}></button>
+      </div>
+    </>
   )
 }
