b842ac947ec36b403cf7bd81eece682d35373893
su1jun <qaxsd530@gmail.com>
2024-07-23 00:07:54 +0900

    add: movelist, hook, movie-json, cards

--- pages/index.tsx
+++ pages/index.tsx
@@ -4,8 +4,8 @@ import { getSession, signOut } from "next-auth/react"
 import useCurrentUser from "@/hooks/useCurrentUser";
 import Navbar from '@/components/Navbar';
 import Billboard from '@/components/Billboard';
-// import MovieList from '@/components/MovieList';
-// import useMovieList from '@/hooks/useMovieList';
+import MovieList from '@/components/MovieList';
+import useMovieList from '@/hooks/useMovieList';
 
 export async function getServerSideProps(context: NextPageContext) {
   const session = await getSession(context);
@@ -25,15 +25,15 @@ export async function getServerSideProps(context: NextPageContext) {
 }
 
 export default function Home() {
-  const { data: user } = useCurrentUser();
+  const { data: movies = [] } = useMovieList();
 
   return (
     <>
       <Navbar />
       <Billboard />
       <div className="pb-40">
-        {/* <MovieList title="Trending Now" data={movies} />
-        <MovieList title="My List" data={favorites} /> */}
+        <MovieList title="Trending Now" data={movies} />
+        {/* <MovieList title="My List" data={favorites} /> */}
       </div>
       {/* <div>
         <h1 className="text-4xl text-green-500">Netflix Clone</h1>
